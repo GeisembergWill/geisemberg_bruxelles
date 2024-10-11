5cb65de875ecf27879236afcab105753dde7c913
GeisembergWill <wgm@live.fr>
2024-10-11 14:09:09 +0200

    exo1 fini

--- AlgoPHP/Partie1/exo1.php
+++ AlgoPHP/Partie1/exo1.php
@@ -9,7 +9,7 @@ Ecrire un algorithme permettant de compter le nombre de caractères contenus dan
 
 <?php
 
-$phrase = "Notre formation DL commmence aujourd'hui";
+$phrase = "Notre formation DL commence aujourd'hui";
 $longueur= strlen($phrase);
-echo "La phrase « $phrase » contient $longueur caractères<br>";
-echo "La phrase « $phrase » contient ".strlen($phrase). " caractères<br>";
+echo "La phrase «$phrase »contient $longueur caractères<br>";
+echo "La phrase «$phrase »contient ".strlen($phrase). " caractères<br>";
