c4f4ff380a9844883cdda55afaa633377eaf31a0
GeisembergWill <wgm@live.fr>
2024-10-11 15:08:35 +0200

    exo 3 fini

---
+++ AlgoPHP/Partie1/exo3.php
@@ -0,0 +1,12 @@
+<h1>Exercice 1 </h1>
+
+
+
+<h2>Résultat</h2>
+
+
+
+<?php
+$phrase = "Notre formation DL commence aujourd'hui";
+echo $phrase."<br>";
+echo str_replace("aujourd'hui","demain",$phrase)."<br>";
