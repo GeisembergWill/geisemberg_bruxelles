ea16a884487f9e6bf355978158437c64fe17297b
GeisembergWill <wgm@live.fr>
2024-10-11 14:52:46 +0200

    exo2 fini

---
+++ AlgoPHP/Partie1/exo2.php
@@ -0,0 +1,15 @@
+<h1>Exercice 2 </h1>
+
+
+
+<h2>Résultat</h2>
+
+
+
+<?php
+
+
+$phrase ="Notre formation DL commence aujourd'hui";
+$nombreDeMots = str_word_count($phrase);
+
+echo " La phrase «$phrase » contient $nombreDeMots mots";
