e4ee4d8fe291f309f177475210bbfd94ed91522b
GeisembergWill <wgm@live.fr>
2024-10-15 14:15:44 +0200

    exo11

---
+++ AlgoPHP/Partie1/exo11.php
@@ -0,0 +1,15 @@
+<h1>Exercice 11</h1>
+
+
+
+<h2>Résultat</h2>
+
+<?php
+
+$tableauMarque = ["Peugeot","Renault","BMW","Mercedes"];
+$tailleTableauMarque = count($tableauMarque);
+echo "Il y a $tailleTableauMarque marques de voitures dans le tableau :<br>";
+echo "$tableauMarque[0]<br>";
+echo "$tableauMarque[1]<br>";
+echo "$tableauMarque[2]<br>";
+echo "$tableauMarque[3]<br>";
